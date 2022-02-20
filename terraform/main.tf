04daa081c4342bc77b5ddc1532e42a3af9720d2d
Blake Renton <elblayko@gmail.com>
2022-02-19 23:31:04 -0600

    Add firewall configuration to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,6 @@
+####################################################################
+# Terraform Configuration
+
 terraform {
   required_providers {
     digitalocean = {
@@ -12,20 +15,35 @@ terraform {
   }
 }
 
+####################################################################
+# Terraform Variables
+
+# Set Digital Ocean API token
 variable "do_token" {
   description = "DigitalOcean API token"
   type = string
   sensitive = true
 }
 
+# Set CloudFlare API key
 variable "cf_key" {
   description = "CloudFlare API key"
   type = string
   sensitive = true
 }
 
-variable "cf_email" {}
-variable "cf_zone_id" {}
+variable "cf_email" {
+  description = "CloudFlare account e-mail address"
+  type = string
+}
+
+variable "cf_zone_id" {
+  description = "CloudFlare DNS zone ID"
+  type = string
+}
+
+####################################################################
+# Provider Configuration
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
@@ -38,7 +56,10 @@ provider "cloudflare" {
   api_key = var.cf_key
 }
 
-# Create a web server
+####################################################################
+# DigitalOCean Configuration
+
+# Create an Ubuntu droplet
 resource "digitalocean_droplet" "blakerenton-net" {
   image       = "ubuntu-21-10-x64"
   name        = "blakerenton-net"
@@ -48,7 +69,93 @@ resource "digitalocean_droplet" "blakerenton-net" {
   ssh_keys    = [ "75:1c:b4:af:e1:70:dc:25:1c:93:0a:7f:e7:ba:70:7c" ]
 }
 
-# Create a CloudFlare record
+# Create DigitalOcean firewall
+# IP Address reference: https://www.cloudflare.com/ips/
+resource "digitalocean_firewall" "blakerenton-net" {
+  name = "cloudflare"
+
+  droplet_ids = [digitalocean_droplet.blakerenton-net.id]
+
+  # HTTP Configuration
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "80"
+    source_addresses = [ 
+    "173.245.48.0/20", 
+    "103.21.244.0/22", 
+    "103.22.200.0/22", 
+    "103.31.4.0/22", 
+    "141.101.64.0/18", 
+    "108.162.192.0/18", 
+    "190.93.240.0/20", 
+    "188.114.96.0/20", 
+    "197.234.240.0/22", 
+    "198.41.128.0/17", 
+    "162.158.0.0/15", 
+    "104.16.0.0/13", 
+    "104.24.0.0/14", 
+    "172.64.0.0/13", 
+    "131.0.72.0/22" ]
+  }
+
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "443"
+    source_addresses = [ 
+    "173.245.48.0/20", 
+    "103.21.244.0/22", 
+    "103.22.200.0/22", 
+    "103.31.4.0/22", 
+    "141.101.64.0/18", 
+    "108.162.192.0/18", 
+    "190.93.240.0/20", 
+    "188.114.96.0/20", 
+    "197.234.240.0/22", 
+    "198.41.128.0/17", 
+    "162.158.0.0/15", 
+    "104.16.0.0/13", 
+    "104.24.0.0/14", 
+    "172.64.0.0/13", 
+    "131.0.72.0/22" 
+    ]
+  }
+
+  # SSH Configuration
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # ICMP Configuration
+  inbound_rule {
+    protocol = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # Outbound Configuration
+  outbound_rule {
+    protocol = "tcp"
+    port_range="1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol = "udp"
+    port_range = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+####################################################################
+# CloudFlare Configuration
+
+# Create root record
 resource "cloudflare_record" "blakerenton-net-a" {
   zone_id = var.cf_zone_id
   name    = "blakerenton.net"
@@ -58,6 +165,7 @@ resource "cloudflare_record" "blakerenton-net-a" {
   proxied = true
 }
 
+# Create www cname record
 resource "cloudflare_record" "blakerenton-net-www" {
   zone_id = var.cf_zone_id
   name    = "www"
@@ -67,6 +175,7 @@ resource "cloudflare_record" "blakerenton-net-www" {
   proxied = true
 }
 
+# Create traefik cname record
 resource "cloudflare_record" "blakerenton-net-traefik" {
   zone_id = var.cf_zone_id
   name    = "traefik"
@@ -76,6 +185,7 @@ resource "cloudflare_record" "blakerenton-net-traefik" {
   proxied = true
 }
 
+# Create status cname record
 resource "cloudflare_record" "blakerenton-net-status" {
   zone_id = var.cf_zone_id
   name    = "status"
@@ -84,6 +194,9 @@ resource "cloudflare_record" "blakerenton-net-status" {
   ttl     = 1 # auto
   proxied = true
 }
+
+####################################################################
+# After completion
 
 # Output the IP address
 output "droplet_ipv4_address" {
